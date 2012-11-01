8a3bda33d53b58310fba0a2ce8f82df43804e00c
holy <holysugar@gmail.com>
2012-11-01 19:16:05 +0900

    Kannsuuji calculate between 10 ... 10000

--- lib/to_chitanda/kansuuji.rb
+++ lib/to_chitanda/kansuuji.rb
@@ -5,11 +5,35 @@ module ToChitanda
   module Kansuuji
 
     KANJI_NUMBERS = %w(〇 一 二 三 四 五 六 七 八 九)
-    LOWER_RANKS   = [''] + %w(十 百 千)
+    LOWER_RANKS   = [nil] + %w(十 百 千)
 
     def kansuuji(number)
       raise ArgumentError, "can't calculate negative number" if number < 0
+
+      number, lower = number.divmod(10000)
+
+      kansuuji_lower_10000(lower)
+    end
+
+    private
+    def kansuuji_lower_10000(number)
       return KANJI_NUMBERS[number] if number <= 9
+      ranks = LOWER_RANKS.dup
+      kanjis = []
+
+      while number > 0
+        number, mod = number.divmod(10)
+        rank = ranks.shift
+        kanji = case mod
+              when 0; ''
+              when 1; rank || KANJI_NUMBERS[1]
+              else  ; "#{KANJI_NUMBERS[mod]}#{rank}"
+              end
+        kanjis << kanji
+      end
+
+      kanjis.reverse.join
+
     end
 
     extend self
--- spec/to_chitanda/kansuuji_spec.rb
+++ spec/to_chitanda/kansuuji_spec.rb
@@ -8,10 +8,20 @@ describe ToChitanda::Kansuuji do
     include ToChitanda::Kansuuji
 
     [
-    #   argument, result
-      [        0,     "〇"],
-      [        1,     "一"],
-      [        2,     "二"],
+    #   argument,      result
+      [        0,       "〇"],
+      [        1,       "一"],
+      [        2,       "二"],
+
+      [       10,           "十"],
+      [       11,         "十一"],
+      [       12,         "十二"],
+      [       20,         "二十"],
+      [       21,       "二十一"],
+      [      100,           "百"],
+      [      321,   "三百二十一"],
+      [     1000,           "千"],
+      [     1321, "千三百二十一"],
 
     ].each do |arg, result|
 
@@ -25,6 +35,7 @@ describe ToChitanda::Kansuuji do
       lambda{ kansuuji(-1) }.must_raise ArgumentError
     end
   end
+
 end
 
 
